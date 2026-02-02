d4f255b6cc044506de338a425d8171564ea44ad8
Kiana Mills <kiana.mills20@gmail.com>
2026-02-01 17:31:47 -0800

    cart

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
         <PlantCard key={plant.id} plant={plant} cartItems={cartItems} setCartItems={setCartItems} ></PlantCard>
       ))}
 
-      <Cart />
+      <Cart cartItems={cartItems} setCartItems={setCartItems} />
     </>
   );
 }
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,3 +1,27 @@
 export default function Cart(){
-    return <h2>Cart</h2>
+    function cartIncrease(id){
+        const copyCartItems = [...props.cartItems];
+        for(let i = 0; i < copyCartItems.length; i++){
+            if(copyCartItems[i].id === id){
+                copyCartItems[i] = {
+                    ...copyCartItems[i], 
+                    quantity: copyCartItems[i].quantity + 1, 
+                }
+            }
+        }
+        props.setCartItems(copyCartItems);
+    }
+    return(
+        <>   
+         <h2>Cart</h2>
+         {props.cartItems.map((i) => {
+            <div>
+            <span>{i.item}</span> 
+            <span>{i.name}</span>
+            <span>{i.quantity}</span>
+            </div>
+         }
+        )}
+         </>
+    );
 }
